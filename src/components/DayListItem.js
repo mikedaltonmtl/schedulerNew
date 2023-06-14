fef6a8512b85f7b5f68770d52c9b2c6102e5cf98
mike-dalton <michael.j.dalton@gmail.com>
2023-06-14 16:53:35 -0400

    add conditional text in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,20 @@ export default function DayListItem({ name, spots, selected, setDay }) {
     "day-list__item--full": spots === 0
   });
 
+  const formatSpots = function() {
+    switch (spots) {
+      case 0: return "no spots remaining";
+      case 1: return "1 spot remaining";
+      default: return `${spots} spots remaining`;
+    }
+  };
+
+  const spotsText = formatSpots();
+
   return (
-    <li className={ dayClass } onClick={() => setDay(name)}>
+    <li className={ dayClass } onClick={ () => setDay(name) }>
       <h2 className="text--regular">{ name }</h2> 
-      <h3 className="text--light">{ spots } spots remaining</h3>
+      <h3 className="text--light">{ spotsText }</h3>
     </li>
   );
 }
