e6503cb1a46993e141171c183cdf84ee3a5f26c9
mike-dalton <michael.j.dalton@gmail.com>
2023-06-27 16:40:06 -0400

    refactor Appointment and add stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import "components/Appointment/styles.scss";
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 
 export default function Appointment(props) {
 
+  const onAddTest = function() {
+    console.log('onAddTest');
+  };
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={ props.time } />
+      { props.interview ?
+        <Show
+          student={ props.interview.student }
+          interviewer={ props.interview.interviewer }
+        />
+        :
+        <Empty onAdd={ onAddTest } />
+      }
+    </article>
   );
 }
