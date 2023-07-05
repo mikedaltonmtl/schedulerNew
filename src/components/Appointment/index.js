200caf52b1e779eaf5ea6f7ff68120cfcd0e531a
mike-dalton <michael.j.dalton@gmail.com>
2023-07-05 09:07:28 -0400

    add CREATE component to appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,26 +3,42 @@ import "components/Appointment/styles.scss";
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
-import { mode, transition, back } from '../../hooks/useVisualMode';
+import Form from './Form';
+
+import useVisualMode from '../../hooks/useVisualMode';
+
 
 
 export default function Appointment(props) {
 
-  const onAddTest = function() {
-    console.log('onAddTest');
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  const onAdd = function() {
+    transition(CREATE);
   };
 
   return (
     <article className="appointment">
       <Header time={ props.time } />
-      { props.interview ?
+      { mode === SHOW && (
         <Show
           student={ props.interview.student }
           interviewer={ props.interview.interviewer }
         />
-        :
-        <Empty onAdd={ onAddTest } />
-      }
+      )}
+      { mode === EMPTY && (
+        <Empty onAdd={ onAdd } />
+      )}
+      { mode === CREATE && (
+        <Form
+          interviewers={ [] }
+          onCancel={ back }
+        />
+      )}
     </article>
   );
 }
