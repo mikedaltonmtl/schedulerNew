5f95abf057934ca309b978c386817c22e4779434
mike-dalton <michael.j.dalton@gmail.com>
2023-07-08 09:36:46 -0400

    add update spots

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
+import useApplicationData from '../hooks/useApplicationData';
 
 /*
 const appointmentsArray = {
@@ -48,74 +48,18 @@ const appointmentsArray = {
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-      day: "Monday",
-      days: [],
-      appointments: {},
-      interviewers: {}
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  const setDay = day => setState({ ...state, day });
+  const interviewers = getInterviewersForDay(state, state.day);
 
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(all => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
-  }, []);
-
-  const bookInterview = function(id, interview) {
-    // Make copy of our appointment object & add the new interview object
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    // Update the appointments array with the appointment we just created
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    // Update the database
-    return axios.put(`/api/appointments/${id}`, {
-      interview: interview
-    })
-    // Update state with this new appointments array
-    .then(() => {
-      setState(prev => ({ ...prev, appointments }));
-    });
-  };
-
-  // To cancel an interview, we need to set the interview object to null
-  const cancelInterview = function(id) {
-    // Make copy of our appointment object & add set the interview object to null
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    // Update the appointments array with the cancelled appointment
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    // Update the database
-    return axios.delete(`/api/appointments/${id}`, {
-      interview: null
-    })
-    // Update state with this new appointments array
-    .then(() => {
-      setState(prev => ({ ...prev, appointments }));
-    });
-  };
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-
-  const schedule = dailyAppointments.map(appointment => {
+  const schedule = getAppointmentsForDay(state, state.day).map(appointment => {
     
     const interview = getInterview(state, appointment.interview);
-    const interviewers = getInterviewersForDay(state, state.day);
   
     return (
       <Appointment
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then(all => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    });
+  }, []);
+
+  // Calculate the new value of available spots for the day in State
+  // It will NOT update state directly, but will return an up to date days array to be put in state
+  const updateDaySpots = function(updatedAppointments) {
+
+    // Get the day object and update the empty spots for the selected day
+    const selectedDay = Object.values(state.days).find(day => day.name === state.day);
+
+    const nbSpots = selectedDay.appointments.filter(appointmentId => {
+      return updatedAppointments[appointmentId].interview === null
+    }).length;
+
+    return state.days.map(day => {
+      if (day.name === selectedDay.name) {
+        day.spots = nbSpots;
+      }
+      return day;
+    });
+  };
+
+  const bookInterview = function(id, interview) {
+    // Make copy of our appointment object & add the new interview object
+    const appointment = { ...state.appointments[id], interview: { ...interview } };
+    // Update the appointments array with the appointment we just created
+    const appointments = { ...state.appointments, [id]: appointment };
+    // Update the database
+    return axios.put(`/api/appointments/${id}`, {
+      interview: interview
+    })
+    .then(() => {
+      // Avoid stale state, pass updated appointments to update the day
+      setState(prev => ({ ...prev, appointments, days: updateDaySpots(appointments)}));
+    });
+  };
+
+  // To cancel an interview, we need to set the interview object to null
+  const cancelInterview = function(id) {
+    // Make copy of our appointment object & add set the interview object to null
+    const appointment = { ...state.appointments[id], interview: null };
+    // Update the appointments array with the cancelled appointment
+    const appointments = { ...state.appointments, [id]: appointment };
+    // Update the database
+    return axios.delete(`/api/appointments/${id}`, {
+      interview: null
+    })
+    // Update state with this new appointments array
+    .then(() => {
+      setState(prev => ({ ...prev, appointments, days: updateDaySpots(appointments)}));
+    });
+  };
+
+  return { state, setDay, bookInterview, cancelInterview };
+};
